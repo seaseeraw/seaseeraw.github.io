a6fd833dd79e86131c900ade7eb0b5d3ed162d6f
shishir <sesseeraw12@gmail.com>
2023-10-08 18:18:36 +1100

    ch

--- src/App.js
+++ src/App.js
@@ -23,19 +23,19 @@
 // }
 
 // export default App;
-import './App.css';
-import Home from './pages/Home/Home';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+// import './App.css';
+// import Home from './pages/Home/Home';
+// import { BrowserRouter, Routes,Route } from 'react-router-dom';
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
