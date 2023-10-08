91ad45107bd29dedb7c785d039da846d0626074f
shishir <sesseeraw12@gmail.com>
2023-10-08 17:56:59 +1100

    change made

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <>
+            <div>About Me</div>
+            <div>
+                <h1>Shishir Pathak</h1>
+                <address>32 fifth street Granville</address>
+                <h1>About Me</h1>
+                <p>Hi, I am Shishir Pathak, a web developer. I live in Sydney. I am looking for work as a web developer. </p>
+            </div>
+        </>
+    )
+}
+
+export default About;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import About from "../About/About";
+import Skills from "../Skills/Skills";
+import { Link } from "react-router-dom";
+import Examples from "../Examples/Examples";
+import Tools from "../Tools/Tools";
+import Loader from "../../components/Loader/Loader";
+
+const Home = () =>{
+    const [isLoading,setIsLoading] = useState(false);
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(true);
+        }, 4000);
+    },[])
+    return(
+        <>
+            {isLoading ? 
+            <div>
+                <About />
+                <Skills />
+                <Examples />
+                <Tools />
+            </div>:
+            <Loader />}
+        </>
+    )
+}
+
+export default Home;
